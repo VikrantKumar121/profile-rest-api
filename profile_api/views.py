815aa88be5a79944f05bebd586d96990b7d630be
Vikrant <mr.vikrantkumar121@gmail.com>
2020-06-22 04:21:38 +0530

    added post in api view and used serializer

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profile_api import serializer
 
 class HelloApiView(APIView):
     '''Test api View'''
+    serializer_class = serializer.HelloSerializer
 
     def get(self, request, format = None):
         '''reaturns a list of api views features'''
@@ -16,3 +18,18 @@ class HelloApiView(APIView):
         ]
 
         return(Response({'message': 'Hello', 'an_apiview' : an_apiview}))
+
+    def post(self, request, format = None):
+        ''''''
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message' : message})
+
+        else:
+
+            return Response(serializer.errors)

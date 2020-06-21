5a99f575927bf79f99cab70d178c90f62a1f9501
Vikrant <mr.vikrantkumar121@gmail.com>
2020-06-21 23:18:12 +0530

    changes in user manager

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,33 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
-
-class UserProfileManager():
+class UserProfileManager(BaseUserManager):
     """manager for user profile"""
 
     def create_user(self, email, name , password = None):
         """Create new user """
         if not email:
             raise ValueError('no eamail provided')
-
-        email = self.normalie_email(email)
+        email = self.normalize_email(email)
         user = self.model(email = email, name = name)
-
         user.set_password(password)
         user.save(using = self._db)
-
         return(user)
 
-    def create_super_user(self, email, user, password):
+    def create_superuser(self, email, name, password):
         """create new superuser"""
         user = self.create_user(email, name, password)
-
         user.is_superuser = True
         user.is_staff = True
         user.save(using = self._db)
-
         return(user)
 
 
@@ -35,7 +30,7 @@ class UserProfileManager():
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     '''database model for users'''
     email = models.EmailField(max_length = 255, unique = True )
-    name = models.CharField(max_length = 255, unique = True)
+    name = models.CharField(max_length = 255)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default = False)
 
